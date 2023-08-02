9920827b93a96ff8c7a21093c2a668688e7ffc2f
Timothy <tidee98@web.de>
2023-08-02 18:37:33 +0200

    - added dapr publisher and subscription controller - added service method to create a user progress event - added method to listen to content deletion event and delete related quizzes

--- src/main/java/de/unistuttgart/iste/gits/quiz_service/persistence/repository/QuizRepository.java
+++ src/main/java/de/unistuttgart/iste/gits/quiz_service/persistence/repository/QuizRepository.java
@@ -4,9 +4,18 @@ import de.unistuttgart.iste.gits.quiz_service.persistence.dao.QuizEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface QuizRepository extends JpaRepository<QuizEntity, UUID> {
+
+    /**
+     * returns all Quiz Entities that contain one of the Assessment IDs provided in the input list
+     *
+     * @param assessmentIds list of assessment IDs
+     * @return all Quiz ENtities matchin one of the provided Assessment IDs
+     */
+    List<QuizEntity> findQuizEntitiesByAssessmentIdIn(List<UUID> assessmentIds);
 
 }
